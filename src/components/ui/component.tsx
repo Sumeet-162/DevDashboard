576f8f465e97755ec81404c74a83e2a420fed4a3
Sumeet-162 <sumeet2703@gmail.com>
2025-09-14 00:31:20 +0530

    11:43

--- src/components/ui/component.tsx
+++ src/components/ui/component.tsx
@@ -539,8 +539,8 @@ const DevDashLandingPage: React.FC = () => {
                     className: index === 0 
                       ? "[grid-area:stack] hover:-translate-y-10 before:absolute before:w-[100%] before:outline-1 before:rounded-xl before:outline-border before:h-[100%] before:content-[''] before:bg-blend-overlay before:bg-background/50 grayscale-[100%] hover:before:opacity-0 before:transition-opacity before:duration-700 hover:grayscale-0 before:left-0 before:top-0"
                       : index === 1
-                      ? "[grid-area:stack] translate-x-8 sm:translate-x-12 translate-y-10 hover:-translate-y-1 before:absolute before:w-[100%] before:outline-1 before:rounded-xl before:outline-border before:h-[100%] before:content-[''] before:bg-blend-overlay before:bg-background/50 grayscale-[100%] hover:before:opacity-0 before:transition-opacity before:duration-700 hover:grayscale-0 before:left-0 before:top-0"
-                      : "[grid-area:stack] translate-x-16 sm:translate-x-24 translate-y-20 hover:translate-y-10"
+                      ? "[grid-area:stack] translate-x-6 sm:translate-x-8 translate-y-10 hover:-translate-y-1 before:absolute before:w-[100%] before:outline-1 before:rounded-xl before:outline-border before:h-[100%] before:content-[''] before:bg-blend-overlay before:bg-background/50 grayscale-[100%] hover:before:opacity-0 before:transition-opacity before:duration-700 hover:grayscale-0 before:left-0 before:top-0"
+                      : "[grid-area:stack] translate-x-12 sm:translate-x-16 translate-y-20 hover:translate-y-10"
                   }))}
                 />
               </motion.div>
